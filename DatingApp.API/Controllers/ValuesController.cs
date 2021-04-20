d62fe0d51c79aa544fc654bab634ad61354753d0
p.nowinski <you@infoneer@infoneer.pl>
2021-04-20 13:25:52 +0200

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingApp.API.Controllers
             this._context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
